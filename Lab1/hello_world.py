76d5548b45f600fac6ee666027a005677bcaacd5
jcomfort1 <joshuacomfort@gmail.com>
2023-02-01 10:29:54 -0500

    Added main to hello_world script

--- Lab1/hello_world.py
+++ Lab1/hello_world.py
@@ -3,4 +3,6 @@
 # File: hello_world.ipynb
 # Description: A simple script to write the text "Hello, world!" to the console
 
-print("Hello, world!") #Wrtie the text "Hello, world!" to the console
+# Main function to specify the start of the script
+if __name__ == "__main__":
+    print("This is my first lab for 118. Excited to learn!") #Write the String to the console
